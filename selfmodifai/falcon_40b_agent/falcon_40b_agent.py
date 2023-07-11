90db39a6d7316b67b76121cbd472372ac4e032c5
Jackson Karel <jackson.karel2@gmail.com>
2023-07-11 13:34:06 -0700

    Falcon 40b agent base prompt

---
+++ selfmodifai/falcon_40b_agent/falcon_40b_agent.py
@@ -0,0 +1,9 @@
+from selfmodifai.falcon_40b_agent import falcon_40b_generate
+
+def falcon_40b_agent():
+    base_prompt = f"""I am a highly intellgient AI agent that is improving the model architecture of the language model Alpaca-LoRA. I write bash commands that are then executed and the outputs are then returned to me. This helps me understand the Alpaca-LoRA codebase and improve the model architecture of Alpaca-LoRA.\n\nTo better understand the Alpaca-LoRA codebase, I should"""
+
+    print(falcon_40b_generate(base_prompt))
+
+    
+    
---
+++ selfmodifai/falcon_40b_agent/falcon_40b_generate.py
@@ -0,0 +1,15 @@
+from transformers import AutoModelForCausalLM, AutoConfig, AutoTokenizer
+import torch
+
+def falcon_40b_generate(input_text):
+    model_path="tiiuae/falcon-40b-instruct"
+
+    config = AutoConfig.from_pretrained(model_path, trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True, load_in_4bit=True, device_map="auto")
+
+    tokenizer = AutoTokenizer.from_pretrained("tiiuae/falcon-40b-instruct")
+
+    input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
+
+    outputs = model.generate(input_ids, max_length=100)
+    return tokenizer.decode(outputs[0])
