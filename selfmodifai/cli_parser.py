db172d8f6bf754e93bee99e849d93719b3188e68
Jackson Karel <jackson.karel2@gmail.com>
2023-10-02 21:38:46 -0700

    Update CLI

--- selfmodifai/cli_parser.py
+++ selfmodifai/cli_parser.py
@@ -1,5 +1,5 @@
 import argparse
-from selfmodifai.agents.hf_agent.llama2_agent.llama2_agent import llama2_agent
+from selfmodifai.agents.open_source_agent.open_source_agent import open_source_agent
 from selfmodifai.agents.openai_agents.openai_func_agent.openai_func_agent import openai_func_agent
 from selfmodifai.agents.openai_agents.gpt4_agent.agents.gpt_4_training_editor_agent.agents.gpt4_te_alpaca_lora_agent import (
     gpt4_te_alapca_lora_agent,
@@ -9,12 +9,12 @@ from selfmodifai.agents.openai_agents.gpt4_agent.agents.gpt_4_training_editor_ag
 def cli_parser():
     sm_parser = argparse.ArgumentParser(description="Autonomous AI agents modifying the training code of ML models")
 
-    sm_parser.add_argument("agent", action="store", choices=["llama2", "gpt4", "gpt4_alpaca_lora"], help="Agent type")
+    sm_parser.add_argument("agent", action="store", choices=["open_source", "gpt4", "gpt4_alpaca_lora"], help="Agent type")
 
     args = sm_parser.parse_args()
 
     agent = args.agent
-    agents = {"llama2": llama2_agent, "gpt4": openai_func_agent, "gpt4_alapca_lora": gpt4_te_alapca_lora_agent}
+    agents = {"open_source": open_source_agent, "gpt4": openai_func_agent, "gpt4_alapca_lora": gpt4_te_alapca_lora_agent}
 
     chosen_agent = agents[agent]
     chosen_agent()
